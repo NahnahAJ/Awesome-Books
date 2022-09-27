ee2f3803391856f351724a6b7c69ce4228186f45
Nahnah Ahjuwah <nahnahahjuwah@gmail.com>
2022-09-27 11:59:56 -0700

    Add function to display books in a different module

---
+++ modules/displaybooks.js
@@ -0,0 +1,18 @@
+const container = document.getElementById("books-list");
+
+const displayBooks = (bookObj) => {
+  const holder = document.createElement("div");
+  const titleAuthor = document.createElement("p");
+  const removeBtn = document.createElement("button");
+
+  titleAuthor.innerText = `"${bookObj.title}" by ${bookObj.author}`;
+  removeBtn.innerText = "Remove";
+  removeBtn.setAttribute("data-id", bookObj.id);
+  removeBtn.className = "btn";
+  titleAuthor.className = "title-author";
+  holder.className = "book-holder";
+  holder.append(titleAuthor);
+  holder.append(removeBtn);
+  container.append(holder);
+};
+export default displayBooks;
